69f6e097c4f68778aa342845532cee3ff8a420e5
Jordan Burns <jordan.burns@gmail.com>
2015-03-23 00:09:22 +0000

    Update to layout & fixed / tested latestpost cms plugin

--- jblog/cms_plugins.py
+++ jblog/cms_plugins.py
@@ -3,7 +3,8 @@ from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext as _ 
 from .models import Post
-
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class LatestPostPlugin(CMSPluginBase):
@@ -13,7 +14,12 @@ class LatestPostPlugin(CMSPluginBase):
     render_template = 'jblog/latest_post.html'
 
     def render(self, context, instance, placeholder):
-        context['latest_post'] = Post.objects.filter(published=True).latest(field_name='published_date')
-        return context
+        try:
+            context['latest_post'] = Post.objects.filter(published=True).latest(field_name='published_date')
+            return context
+        except ObjectDoesNotExist:
+            context['latest_post'] = None
+            return context
+
 
 plugin_pool.register_plugin(LatestPostPlugin)
